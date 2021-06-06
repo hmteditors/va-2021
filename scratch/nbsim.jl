a2f00cc790d7c6c095474613207c8e45e1fdcc8e
Neel Smith <dnsmith.neel@gmail.com>
2021-06-06 15:26:10 -0400

    Add standard files for summer '21 work

--- scratch/nbsim.jl
+++ scratch/nbsim.jl
@@ -1,23 +1,39 @@
+# Simulate what happens in the Pluto validating notebook.
+using EditorsRepo
 using CitableText
 using CitableObject
-using CitableImage
-using CitableTeiReaders
-using CSV
-using DataFrames
-using EditionBuilders
-using EditorsRepo
-using HTTP
-#using Lycian
-using Markdown
-using Orthography
 using ManuscriptOrthography
-using PolytonicGreek
+using Orthography
+
+reporoot = pwd()
+repo = repository(reporoot)
+
+## CATALOGING CHECKS: (works in notebook)
+cites = citation_df(repo)
+filesmatch(trepo, cites)
+
+# ... document other functions listing discrepancies
+
+## SURFACES FOR MENU (works in notebook)
+surfs = surfaces(repo)
+surface = surfs[1]
 
 
+## DSE COMPLETENESS (works in notebook)
+sdse = surfaceDse(repo, surface)
+# Use citableimage library to craft URLs to ICT2.
 
-repo = EditingRepository(pwd(), "editions", "dse", "config")
-surf = Cite2Urn("urn:cite2:hmt:msA.v1:100v")
+## DSE ACCURACY (works in notebook)
+row = sdse[1, :]
+diplnodetext(repo, row.passage)
 
-sdse = EditorsRepo.surfaceDse(repo, surf)
-u = sdse[1,1]
-diplomaticnodes(repo, u)
+## ORTHOGRAPHY (works in notebook)
+textconfig = citation_df(repo)
+
+tidy = EditorsRepo.baseurn(row.passage)
+
+# Get the orthography
+ortho = orthographyforurn(textconfig, tidy)
+chunks = normednodetext(repo, row.passage) |> split
+
+validstring(ortho, chunks[1])
